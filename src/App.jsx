6dbb5cde20eb8ab31e926004ec413b5ca273f7e1
Murilonszz22 <rm95234@estudante.fieb.edu.br>
2025-03-21 09:57:33 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import {useState} from "react"
 function App(){
   const [nome,setNome] = useState('Ronaldo')
 
-
-  return(
+const Formulario = ()=>{
+  return (
     <div>
-      <h3>Pizzaria 2E</h3>
-
-      <input 
+<input 
       className = "nome"
       onChange={(e)=>{setNome(e.target.value)}}
       type="text" />
@@ -21,6 +19,20 @@ function App(){
       >
           Clique aqui
       </button>
+    </div>
+
+  )
+}
+  return(
+    <div>
+      <h3>Pizzaria 2E</h3>
+
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+
+      
 
     </div>
   )
